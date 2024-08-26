814a1fd55d9957d9cb2ae457e7d8389b826b60b8
josuechevez1998 <josuechevez977@gmail.com>
2024-08-25 22:46:28 -0600

    feat navigation component

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -53,28 +53,30 @@ new class extends Component {
                 @endrole
 
                 @hasanyrole('System|Admin')
-                    <div class="hidden space-x-2 sm:-my-px sm:ms-10 sm:flex">
-                        <div x-data="{ open: false }" class="relative">
-                            <button @click="open = ! open"
-                                class="inline-flex items-center px-2 py-2 pt-6 border border-transparent text-sm font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150 text-center">
-                                {{ __('Settings') }}
-                                {{ __('Types') }}
-                                <svg class="ml-2 h-5 w-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                        d="M19 9l-7 7-7-7"></path>
-                                </svg>
-                            </button>
+                    <div class="hidden sm:flex sm:items-center sm:ms-10">
+                        <x-dropdown align="left" width="48">
+                            <x-slot name="trigger">
+                                <button
+                                    class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
+                                    <span>{{ __('Admin') }}</span>
 
-                            <div x-show="open" @click.away="open = false"
-                                class="absolute left-0 mt-1 w-48 bg-white border border-gray-200 rounded-md shadow-lg z-10">
-                                <div class="py-1 text-center">
-                                    <a href="{{ route('types-customers.index') }}"
-                                        class="block px-3 py-2 text-sm text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
-                                        {{ __('Customers') }}
-                                    </a>
-                                </div>
-                            </div>
-                        </div>
+                                    <div class="ms-1">
+                                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg"
+                                            viewBox="0 0 20 20">
+                                            <path fill-rule="evenodd"
+                                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                                clip-rule="evenodd" />
+                                        </svg>
+                                    </div>
+                                </button>
+                            </x-slot>
+
+                            <x-slot name="content">
+                                <x-dropdown-link :href="route('types-customers.index')" :active="request()->routeIs('types-customers.index')" wire:navigate>
+                                    {{ __('Types Customers') }}
+                                </x-dropdown-link>
+                            </x-slot>
+                        </x-dropdown>
                     </div>
                 @endhasanyrole
 
